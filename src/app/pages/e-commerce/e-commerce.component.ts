9b365770131a9d319d37de461115c7ef4bc659c7
mnt <swastiksupadhye@gmail.com>
2019-02-26 20:27:12 +0530

    excel read data

--- src/app/pages/e-commerce/e-commerce.component.ts
+++ src/app/pages/e-commerce/e-commerce.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import * as firebase from 'firebase/app';
 import { Router,ActivatedRoute } from "@angular/router";
+import * as XLSX from 'ts-xlsx';
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';  
 
 @Component({
@@ -25,6 +26,9 @@ export class ECommerceComponent {
 	private allField = false;
 	private value = "";
 	private optionSelect = false;
+	private journeyTypeData = [{id: 1, name: 'Train'},{id: 2, name:'Flight'}]
+	arrayBuffer:any;
+	file:File;
 	constructor(private db: AngularFireDatabase,private router: Router){
 		/*var user = JSON.parse(localStorage.getItem("user"));
 		console.log("user",user);
@@ -34,12 +38,104 @@ export class ECommerceComponent {
         }*/
 	}
 
+	//reading file form local path
+	incomingfile(event) {
+	  this.file= event.target.files[0]; 
+	}
+
+	// reading  data from excel and uploading data in firebase
+	Upload() {
+		var me = this;
+		var group_id = me.createGroupId();
+		var date = new Date();
+        var dateCreated = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+	    let fileReader = new FileReader();
+        fileReader.onload = (e) => {
+            this.arrayBuffer = fileReader.result;
+            var data = new Uint8Array(this.arrayBuffer);
+            var arr = new Array();
+            for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+            var bstr = arr.join("");
+            var workbook = XLSX.read(bstr, {type:"binary", cellDates: true});
+            var first_sheet_name = workbook.SheetNames[0];
+            var worksheet = workbook.Sheets[first_sheet_name];
+            console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+        	var groupChatData = XLSX.utils.sheet_to_json(worksheet)
+        	
+        	//looping for save data in firebase in group table
+        	for (var i = 0; i < groupChatData.length; i++) {
+		     	console.log("groupchat data", groupChatData[i]);
+		     	console.log('plus time',groupChatData[i]['Chat room duration']);
+		     	console.log('journyType', me.journyType);
+		     	if (me.journyType== 'Train') {
+		     		console.log('if journyType', me.journyType, 'start time', startTime)
+		     		var startTime = me.getStartTime('3:00', groupChatData[i]['Train departure time'] );
+		     		var endTime = me.getEndTime(groupChatData[i]['Train departure time'], groupChatData[i]['Chat room duration']);
+		     		var number = groupChatData[i]['Train number']
+		     	} else if (me.journyType== 'Flight') {
+		     		var startTime = me.getStartTime('4:00', groupChatData[i]['Departure time'] );
+		     		var endTime = me.getEndTime(groupChatData[i]['Departure time'], groupChatData[i]['Chat room duration']);
+		     		var number = groupChatData[i]['Flight number']
+		     		console.log(' else journyType', me.journyType,'start time', startTime)
+		     	}
+		     	
+		     	console.log('endTime...', endTime)
+		     	this.db.list('/Group').push({
+		            DateCreated :dateCreated,
+					endTime : endTime,
+					groupId: group_id,
+					groupName : me.roomName,
+					lastMessage : "",
+					startTime : startTime
+					trainNumber : number,
+					tripeDate : "",
+					unreadCount: 0,
+					type : me.journyType,
+		        });
+		    }
+        }
+        fileReader.readAsArrayBuffer(this.file);
+	}
+
 	ngOnInit(){
 		this.db.list('users').valueChanges().subscribe(user=>{
 			console.log(user);
 		});
 	}
 
+	//calculating chat room start time
+	getStartTime(chatRoomStartTime, DepatureTime) {
+		var start = "25:11";
+		var end = "15:11";
+		var a = chatRoomStartTime.split(":");
+		var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 ; 
+		var b = DepatureTime.split(":");
+		var seconds2 = (+b[0]) * 60 * 60 + (+b[1]) * 60; 
+
+		var date = new Date(1970,0,1);
+		    date.setSeconds(seconds - seconds2);
+
+		var c = date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+		return c;
+		console.log(c);
+	}
+
+	//calculating chat room end time
+	getEndTime(DepatureTime, chatRoomDuration) {
+		var start = "25:11";
+		var end = "15:11";
+		var a = DepatureTime.split(":");
+		var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 ; 
+		var b = chatRoomDuration.split(":");
+		var seconds2 = (+b[0]) * 60 * 60 + (+b[1]) * 60; 
+
+		var date = new Date(1970,0,1);
+		    date.setSeconds(seconds + seconds2);
+
+		var c = date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+		return c;
+		console.log(c);
+	}
 	option(text){
 		if(text == "Train"){
 			this.value1 = true
